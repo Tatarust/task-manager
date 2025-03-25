4faccc74439e406e3e7704d7fab81fef3fc7108d
Tatarplusplus <romanbaygelden26@gmail.com>
2025-03-25 20:19:50 +0300

    save

--- src/main.rs
+++ src/main.rs
@@ -3,13 +3,23 @@ use sqlx::{Pool, Postgres};
 use tokio::runtime::Runtime;
 
 struct Task {
-    id: i64,
+    id: i32,
     task: String,
+}
+
+impl Task {
+    fn new(id: i32, task: String) -> Self {
+        Self {
+            id,
+            task,
+        }
+    }
 }
 
 struct App {
     pool: Option<Pool<Postgres>>,
-    tasks: Vec<String>,
+    tasks: Vec<Task>,
+    runtime: Runtime,
 }
 
 impl App {
@@ -20,6 +30,7 @@ impl App {
         Self {
             pool: Some(pool),
             tasks: Vec::new(),
+            runtime,
         }
     }
 
@@ -42,12 +53,23 @@ impl App {
 
     async fn load_tasks(&mut self) {
         if let Some(pool) = &self.pool {
-            let rows = sqlx::query!("SELECT task FROM tasks")
+            let rows = sqlx::query!("SELECT task, id FROM tasks")
                 .fetch_all(pool)
                 .await
                 .expect("Failed to fetch tasks");
 
-            self.tasks = rows.into_iter().map(|row| row.task).collect();
+            self.tasks = rows.into_iter().map(|row| Task::new(row.id, row.task)).collect();
+        }
+    }
+
+    async fn remove_task(&mut self, task_id: i32) {
+        if let Some(pool) = &self.pool {
+            sqlx::query!("DELETE FROM tasks WHERE id = $1", task_id)
+                .execute(pool)
+                .await
+                .expect("Failed to delete task");
+
+            self.load_tasks().await;
         }
     }
 }
@@ -56,12 +78,32 @@ impl eframe::App for App {
     fn update(&mut self, ctx: &egui::Context, frame: &mut eframe::Frame) {
         egui::CentralPanel::default().show(ctx, |ui| {
             if ui.button("Load tasks").clicked() {
-                let runtime = Runtime::new().unwrap();
-                runtime.block_on(self.load_tasks())
+                let pool = self.pool.clone();
+                let ctx = ctx.clone();
+                self.runtime.spawn(async move {
+                    self.load_tasks();
+                    ctx.request_repaint();
+                });
             }
+
+            let mut remove_id: Option<i32> = None;
             
             for task in &self.tasks {
-                ui.label(task);
+                ui.horizontal(|ui| {
+                    ui.label(format!("{}: {}", task.id, task.task));
+
+                    if ui.button("Remove").clicked() {
+                        remove_id = Some(task.id);
+                    }
+                });
+            }
+
+            if let Some(id) = remove_id {
+                let pool = self.pool.clone();
+                let ctx = ctx.clone();
+                self.runtime.spawn(async move {
+                    if 
+                });
             }
         });
         
