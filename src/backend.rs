6275bc44d8f0e5df7f87f97bf9949311324e36d5
Tatarplusplus <romanbaygelden26@gmail.com>
2025-03-26 13:05:40 +0300

    Separate modules

---
+++ src/backend.rs
@@ -0,0 +1,75 @@
+use sqlx::{Postgres, PgPool, Pool};
+use tokio::runtime::Handle;
+
+pub struct Task {
+    id: i32,
+    description: String,
+}
+
+impl Task {
+    fn new(id: i32, description: String) -> Self {
+        Self {
+            id,
+            description,
+        }
+    }
+
+    pub fn id(&self) -> i32 {
+        self.id
+    }
+
+    pub fn description(&self) -> &str {
+        &self.description
+    }
+
+}
+
+pub struct Backend {
+    pub runtime: Handle,
+    pool: PgPool,
+}
+
+impl Backend {
+    pub fn new(runtime: Handle, database_url: &str) -> Self {
+        let pool: Pool<Postgres> = runtime.block_on(async {
+            PgPool::connect(database_url).await.expect("Error: connect")
+        });
+        Self { runtime, pool }
+    }
+    
+    pub fn load_tasks(&self) -> Vec<Task> {
+        let pool: Pool<Postgres> = self.pool.clone();
+
+        self.runtime.block_on(async move {
+            let rows = sqlx::query!("SELECT task, id FROM tasks")
+                .fetch_all(&self.pool)
+                .await
+                .expect("Failed to load tasks");
+            
+            rows.into_iter().map(|row| Task::new(row.id, row.task)).collect()
+        })
+    }
+
+    pub fn add_task(&self, description: String) {
+        let pool: Pool<Postgres> = self.pool.clone();
+
+        self.runtime.spawn(async move {
+            sqlx::query!("INSERT INTO tasks (task) VALUES ($1)", description)
+                .execute(&pool)
+                .await
+                .ok();
+        });
+    }
+
+    pub fn remove_task(&self, id: i32) {
+        let pool: Pool<Postgres> = self.pool.clone();
+        self.runtime.spawn(async move {
+            if let Err(e) = sqlx::query!("DELETE FROM tasks WHERE id = $1", id)
+                .execute(&pool)
+                .await
+            {
+                eprintln!("Error remove: {:?}", e);
+            }
+        });
+    }
+}
---
+++ src/frontend.rs
@@ -0,0 +1,66 @@
+use crate::backend::{Backend, Task};
+use egui::{CentralPanel, Context, Ui};
+use eframe::Frame;
+pub struct App {
+    backend: Backend,
+    tasks: Vec<Task>,
+    text_input: String,
+}
+
+impl App {
+    pub fn new(backend: Backend) -> Self {
+        let tasks: Vec<Task> = backend.load_tasks();
+        
+        Self {
+            backend,
+            tasks,
+            text_input: String::new(),
+        }
+    }
+}
+
+impl eframe::App for App {
+    fn update(&mut self, ctx: &Context, _frame: &mut Frame) {
+        CentralPanel::default().show(ctx, |ui: &mut Ui| {
+            let backend: &Backend = &self.backend;
+            let tasks: &mut Vec<Task> = &mut self.tasks;
+            let text_input: &mut String = &mut self.text_input;
+            let mut id_to_remove: Option<i32> = None;
+
+            ui.heading("Tasks");
+
+            for task in tasks.iter() {
+                ui.horizontal(|ui: &mut Ui| {
+                    ui.label(format!("{}: {}", task.id(), task.description()));
+
+                    if ui.button("Remove").clicked() {
+                        id_to_remove = Some(task.id());
+                        backend.remove_task(task.id());
+                    }
+                });
+            }
+
+            if let Some(id) = id_to_remove {
+                tasks.retain(|task: &Task| task.id() != id);
+            }
+
+            ui.separator();
+            ui.label("Add");
+            ui.horizontal(|ui: &mut Ui| {
+                ui.text_edit_singleline::<String>(text_input);
+
+                if ui.button("Add").clicked() {
+                    if !text_input.trim().is_empty() {
+                        backend.runtime.block_on(async {
+                            backend.add_task(text_input.clone());
+                            ctx.request_repaint();
+                        });
+                        *tasks = backend.load_tasks();
+                        text_input.clear();
+                    } 
+                }
+            });
+            ctx.request_repaint();
+        });
+    }
+}
